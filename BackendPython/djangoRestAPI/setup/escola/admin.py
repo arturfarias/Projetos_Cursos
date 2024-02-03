83514e0dd11bb9f771badb0b12e33b947d7e8b64
arturfarias <artur06farias@gmail.com>
2024-02-03 02:14:08 -0300

    djangoAPI admin

--- BackendPython/djangoRestAPI/setup/escola/admin.py
+++ BackendPython/djangoRestAPI/setup/escola/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from escola.models import Aluno, Curso
 
-# Register your models here.
+class Alunos(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'rg', 'cpf', 'data_nascimento' )
+    # campos que devem aparecer.
+    list_display_links = ('id', 'nome')
+    # informa quais campos devem ser clicaveis pelo editor
+    search_fields = ['nome',]
+    # quais campos são possiveis de busca
+    list_per_page = 20 # quantidade de elementos por pagina
+
+admin.site.register(Aluno, Alunos) # (model, modelAdmin)
+
+class Cursos(admin.ModelAdmin):
+    list_display = ('id', 'codigo_curso', 'descricao')
+    list_display_links = ('id', 'codigo_curso')
+    search_fields = ('codigo_curso',)
+
+admin.site.register(Curso, Cursos)
